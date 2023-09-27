764dc236f3ccb81f7df9380633acf80f88b26134
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-27 16:18:09 -0600

    Initial Commit

---
+++ MetaUnitTestingInSwift/MetaUnitTestingInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaUnitTestingInSwiftApp.swift
+//  MetaUnitTestingInSwift
+//
+//  Created by Mitch Andrade on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MetaUnitTestingInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
